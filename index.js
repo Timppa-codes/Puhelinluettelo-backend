9a4fd03381bcd39aee0372515093f2bedf7c26ee
Timppa-codes <ihalainen.timo@gmail.com>
2025-02-03 10:05:24 +0200

    puhelinluettelo backend

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    }
+]
+
+app.get('/api/info', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people <br></br>${Date()}`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+//id:n avulla haettavan kontaktin route
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+//id:n avulla haettavan kontaktin poiston route
+app.delete('/api/persons/:id', (request, response) => {
+    console.log("käyttäjä poisti id:", request.params.id)
+    response.status(204).end()
+})
+
+//kontaktien lisäykseen käytettävä route
+const generateId = () => {
+    const newId = Math.floor((Math.random() * 1000) + 1)
+    return newId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    //käydään tietokanta läpi ja tallennetaan tiedot muuttujaan, jos täydellinen mätsi löydetään
+    const notSoUnique = persons.find(person => person.name === body.name)
+    //testataan löytyykö ja palautetaan sopiva status error viesteineen
+    if (notSoUnique) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
